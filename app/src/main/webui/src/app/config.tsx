c515177c1ce37a8bf6ff04ff317fc834841c86f2
James Falkner <schtool@gmail.com>
2024-06-06 10:13:59 -0400

    Update config.tsx to not require hard-code hostname

--- app/src/main/webui/src/app/config.tsx
+++ app/src/main/webui/src/app/config.tsx
@@ -1,5 +1,5 @@
 const config = {
-    backend_api_url: process.env.BACKEND_API_URL || 'http://localhost:8005/api',
+    backend_api_url: process.env.BACKEND_API_URL || window.location.protocol + '//' + window.location.host + '/api',
 };
 
 export default config;
