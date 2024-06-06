d8e13042ea670b2daeafa7c5ae2e733a507292bd
Eric Deandrea <eric.deandrea@gmail.com>
2024-06-06 15:04:53 -0400

    Some cleanup and adding some tests

--- app/src/main/java/org/parasol/model/ClaimBotQuery.java
+++ app/src/main/java/org/parasol/model/ClaimBotQuery.java
@@ -1,11 +1,5 @@
 package org.parasol.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 public record ClaimBotQuery(String claim, String query) {
-
-        @JsonCreator
-        public ClaimBotQuery {
-        }
 
 }
--- app/src/main/java/org/parasol/model/ClaimBotQueryResponse.java
+++ app/src/main/java/org/parasol/model/ClaimBotQueryResponse.java
@@ -1,11 +1,5 @@
 package org.parasol.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 public record ClaimBotQueryResponse(String type, String token, String source) {
-
-        @JsonCreator
-        public ClaimBotQueryResponse {
-        }
 
 }
