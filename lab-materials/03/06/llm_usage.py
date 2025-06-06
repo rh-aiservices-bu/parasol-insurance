19e2000248607acb707f734f2e2f410a6a1a2db8
Guillaume Moutier <guillaume.moutier@gmail.com>
2025-06-06 16:19:53 -0400

    workbench + pipelines + models update
    - update of Python libraries in workbenches and pipelines
    - change Flan-T5 for Qwen-2.5-0.5B quantized on vLLM
    - update pipelines code
    - rebuild workbench and pipeline images

--- lab-materials/03/06/llm_usage.py
+++ lab-materials/03/06/llm_usage.py
@@ -9,10 +9,11 @@ TOP_P = 0.95
 TEMPERATURE = 0.01
 PRESENCE_PENALTY = 1.03
 
-def infer_with_template(input_text, template, max_tokens = MAX_NEW_TOKENS):
+
+def infer_with_template(input_text, template, max_tokens=MAX_NEW_TOKENS):
     llm = VLLMOpenAI(
         openai_api_key="EMPTY",
-        openai_api_base= f"{INFERENCE_SERVER_URL}/v1",
+        openai_api_base=f"{INFERENCE_SERVER_URL}/v1",
         model_name="granite-3-1-8b-instruct",
         max_tokens=max_tokens,
         top_p=TOP_P,
@@ -21,24 +22,26 @@ def infer_with_template(input_text, template, max_tokens = MAX_NEW_TOKENS):
         streaming=False,
         verbose=False,
     )
-    
+
     prompt = PromptTemplate(input_variables=["car_claim"], template=template)
-    
+
     conversation = prompt | llm
-    
+
     return conversation.invoke(input=input_text)
-    
+
+
 def similarity_metric(predicted_text, reference_text):
     embedding_model = HuggingFaceEmbeddings()
     evaluator = load_evaluator("embedding_distance", embeddings=embedding_model)
     distance_score = evaluator.evaluate_strings(prediction=predicted_text, reference=reference_text)
-    return 1-distance_score["score"]
-    
+    return 1 - distance_score["score"]
+
+
 if __name__ == '__main__':
     with open('example_text.txt') as f:
         input_text = f.read()
-    
+
     with open('template.txt') as f:
         template = f.read()
-    
+
     infer_with_template(input_text, template)
